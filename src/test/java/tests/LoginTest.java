fc3a00feede9fafe5fa0bd76ac84048a3c6e57e2
Borodich Timofei <borodich.timofei@gmail.com>
2025-05-05 22:02:51 +0300

    added POM

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void checkSuccessLogin() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        assertEquals(productsPage.getTitle(),
+                "Products",
+                "Логин не выполнен");
+    }
+
+    @Test
+    public void checkLoginWithEmptyPassword() {
+        loginPage.open();
+        loginPage.login("standard_user", "");
+        assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Password is required",
+                "SO BAAAAD");
+    }
+
+    @Test
+    public void checkLoginWithWrongPassword() {
+        loginPage.open();
+        loginPage.login("standard_user", "1243143143");
+        assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Username and password do not match any user in this service",
+                "SO BAAAAD");
+    }
+}
